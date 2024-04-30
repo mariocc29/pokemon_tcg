363a89377731cf99358caa79523c672e9c36eb04
Mario Contreras <mariocc29@gmail.com>
2024-04-30 14:11:54 -0400

    Defining styles classes

---
+++ src/pages/main/Main.tsx
@@ -0,0 +1,12 @@
+import './main.scss'
+
+export const Main = () => {
+  return (
+    <>
+      <section className="wrapper">
+        <header>Header</header>
+        <main>Main</main>
+      </section>
+    </>
+  )
+}
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @import "./src/styles/_variables.scss";
+          @import "./src/styles/_fonts.scss";
+        `
+      }
+    }
+  },
 })
