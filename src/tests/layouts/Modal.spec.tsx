b6cbb403e3a85c5073a24f86434c4b041ffbf0de
Mario Contreras <mariocc29@gmail.com>
2024-05-02 19:28:36 -0400

    added unit test on header layouts

---
+++ src/tests/layouts/Modal.spec.tsx
@@ -0,0 +1,16 @@
+// import { render, fireEvent } from '@testing-library/react';
+// import { Modal } from '@/layouts';
+
+describe('Modal', () => {
+  it('should render modal correctly with success status', () => {
+    
+  });
+
+  it('should render modal correctly with alert status', () => {
+    
+  });
+
+  it('should dispatch toggleModal action and navigate to home page when button is clicked', () => {
+    
+  });
+});
